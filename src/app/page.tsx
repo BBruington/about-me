c5a19e0e4af41b20178b247db425780f73e197ae
BIBruington <psychological_chemist@hotmail.com>
2023-12-05 19:45:11 -0600

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-secondary-foreground rounded-lg m-12">
+      <header className="w-5/6 bg-primary p-8 m-12 rounded-xl">
+        <div className="text-white w-full">
+          <h1 className=" text-[70px]">Benjamin Bruington</h1>
+          <h2 className="text-3xl italic">Full-Stack Web Developer</h2>
+
+        </div>
+      </header>
     </main>
   )
 }
