1a778316f60140cf1aa110dd0c54d56b9ac0a2d1
BIBruington <psychological_chemist@hotmail.com>
2023-12-09 02:07:50 -0600

    about me ui

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -1,10 +1,9 @@
-
 export default function AboutMe() {
   return (
-    <div className="flex w-full bg-secondary-foreground p-10 rounded-md">
-      
+    <div className="flex flex-col lg:flex-row w-full bg-secondary-foreground p-10 rounded-md">
+
       {/* About Me */}
-      <div className="ml-8 w-1/3 border-r-2 border-primary p-3">
+      <div className="ml-8 lg:w-1/3 border-b-2 lg:border-b-0 lg:border-r-2 border-primary p-5">
         <h2 className="text-primary mb-3 text-3xl">About me</h2>
         <h3 className="text-primary mb-1">Synopsis</h3>
         <p className="text-white mb-5 leading-8">
@@ -24,8 +23,8 @@ export default function AboutMe() {
             Quest Bazaar
           </a>
           <span> </span>and I&apos;ve experience teaching the fundementals of
-          React and Nextjs. I coaberate and gain experience on a daily basis
-          with senior developers and keep updated with the newest technologies.
+          React and Nextjs. I work with senior developers daily and keep updated
+          with the newest technologies.
         </p>
 
         <h3 className="text-primary mb-1">What I Enjoy</h3>
@@ -37,10 +36,49 @@ export default function AboutMe() {
           <li>Learning languages like German</li>
         </ul>
       </div>
-      <div className="w-1/3 border-r-2 border-primary"></div>
+
+      {/* My Projects */}
+      <div className="lg:w-1/3 border-b-2 lg:border-b-0 lg:border-r-2 border-primary p-5">
+        <h2 className="text-primary mb-3 text-3xl">What I Have Worked On</h2>
+        <ul className="text-white space-y-2 list-disc ml-5">
+          <h3 className="text-primary mb-1">
+            <a
+              target="_blank"
+              href="https://quest-bazaar.vercel.app"
+              className="underline hover:cursor-pointer hover:text-slate-500"
+            >
+              Quest Bazaar
+            </a>
+          </h3>
+          <li className="mt-2">D&D social media app</li>
+          <li className="mt-2">Find and schedule D&D sessions</li>
+          <li className="mt-2">Advertise your game to find new players</li>
+          <h3 className="text-primary mb-1"><a
+            target="_blank"
+            href="https://discover-coffee-sigma.vercel.app/"
+            className="underline hover:cursor-pointer hover:text-slate-500"
+          >
+            Discover Coffee
+          </a></h3>
+          <li>Discover nearby coffee selling shops</li>
+          <li>Uses Foursquare to geolocate nearby coffee shops</li>
+          <h3 className="text-primary mb-1">
+            <a
+              target="_blank"
+              href="https://discover-videos-one.vercel.app/"
+              className="underline hover:cursor-pointer hover:text-slate-500"
+            >
+              Discover Videos
+            </a>
+          </h3>
+          <li>Watch videos using the youtube api</li>
+          <li>favorite and save videos to watch later</li>
+          <li>uses magic auth for two factor authentication</li>
+        </ul>
+      </div>
 
       {/* My Skills */}
-      <div className="flex flex-col w-1/3 p-8">
+      <div className="flex flex-col lg:w-1/3 p-5">
         <h2 className="text-primary text-3xl">My Skills</h2>
         <div className="grid grid-cols-2 text-white mt-3 space-y-3">
           <span className="mt-3">Javascript</span>
@@ -69,9 +107,14 @@ export default function AboutMe() {
           <span>Node js</span>
           <span>Git</span>
           <span>VSCode</span>
+          <span>Foursquare</span>
+          <span>Airtable</span>
+          <span>Magic Auth</span>
+          <span>Jsonwebtokens</span>
+          <span>Hasura cloud</span>
+          <span>Unsplash</span>
         </div>
-      d</div>
-      
+      </div>
     </div>
   );
 }
